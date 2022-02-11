ac847e9019b51095ec84e499beefb8443d3c9f89
leo <celsolnv27@gmail.com>
2022-02-11 11:45:50 -0300

    Preparando revisão de código

--- src/components/Form/index.js
+++
@@ -1,8 +0,0 @@
-import Button from "./Button";
-import Container from "./Container";
-import Form from "./Form";
-import Input from "./Input";
-import StyledLink from "./StyledLink";
-import Title from "./Title";
-
-export { Button, Container, Form, Input, StyledLink, Title };
--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Login from "./Login";
-import SignUp from "./SignUp";
-import Main from "./Main"
-import AddIncome from "./AddIncome";
-import AddExpense from "./AddExpense"
-
-export { Login, SignUp, Main, AddIncome, AddExpense };
